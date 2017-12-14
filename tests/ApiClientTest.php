468fbc026b85587d31648664fb1881be1cf4dd54
pxgamer <owzie123@gmail.com>
2017-12-14 13:56:59 +0000

    Added ApiClientTest class

---
+++ tests/ApiClientTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace YeTii\RhymeGenerator;
+
+use PHPUnit\Framework\TestCase;
+
+class ApiClientTest extends TestCase
+{
+    /**
+     * @var ApiClient
+     */
+    protected $apiClient;
+
+    public function setUp()
+    {
+        $this->apiClient = new ApiClient();
+    }
+
+    public function testCanConstructApiClientClass()
+    {
+        $this->apiClient = new ApiClient();
+
+        $this->assertInstanceOf(ApiClient::class, $this->apiClient);
+    }
+
+    public function testCanConstructApiClientClassWithCacheLifetime()
+    {
+        $this->apiClient = new ApiClient([
+            'cache_lifetime' => 0,
+        ]);
+
+        $this->assertInstanceOf(ApiClient::class, $this->apiClient);
+    }
+
+    public function testCanSetSingleOptionSuccessfully()
+    {
+        $this->apiClient->setOpt(RhymeOpt::SPELLED_LIKE, 'elepant');
+
+        $this->assertEquals('elepant', $this->apiClient->getParameter(RhymeOpt::SPELLED_LIKE));
+    }
+
+    public function testCanSetMultipleOptionSuccessfully()
+    {
+        $this->apiClient->setOpts([
+            RhymeOpt::SPELLED_LIKE => 'elepant',
+            RhymeOpt::SOUNDS_LIKE  => 'elepant',
+        ]);
+
+        $this->assertEquals('elepant', $this->apiClient->getParameter(RhymeOpt::SPELLED_LIKE));
+        $this->assertEquals('elepant', $this->apiClient->getParameter(RhymeOpt::SOUNDS_LIKE));
+    }
+
+    public function testGetWordsReturnsEmptyWhenNoOptionsSpecified()
+    {
+        $this->apiClient->getWords();
+
+        $data = $this->apiClient->getResult();
+
+        $this->assertEmpty($data);
+    }
+
+    public function testGetParameterReturnsNullWithInvalidOption()
+    {
+        $this->assertNull($this->apiClient->getParameter('notActuallyReal'));
+    }
+
+    public function testCanGetWordsWithOption()
+    {
+        $this->apiClient->setOpt(RhymeOpt::SPELLED_LIKE, 'elepant');
+        $this->apiClient->getWords();
+
+        $data = $this->apiClient->getResult();
+
+        $this->assertNotEmpty($data);
+    }
+
+    public function testCanGetWordsWithOptionMethod()
+    {
+        $this->apiClient->spelledLike('elepant');
+        $this->apiClient->getWords();
+
+        $data = $this->apiClient->getResult();
+
+        $this->assertNotEmpty($data);
+    }
+}
