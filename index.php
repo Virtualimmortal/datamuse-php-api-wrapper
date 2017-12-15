3f167b13f8ab45645ea14bbd30933ce3c71fd18c
UnDeAd-YeTii <bradietilley97@gmail.com>
2017-12-15 11:15:01 +0800

    Added cache to gitignore

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors', 'On');
+ini_set('html_errors', 0);
+error_reporting(E_ALL);
+print '<pre>';
+
+include (__DIR__.'/vendor/autoload.php');
+use YeTii\RhymeGenerator\RhymeOpt;
+use YeTii\RhymeGenerator\ApiClient;
+
+$client = new ApiClient();
+$client->spelledLike('elepant')->getWords();//setOpt(RhymeOpt::SPELLED_LIKE, 'elepant')->getWords();
+print_r($client);
+
+die();
